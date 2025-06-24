c1ec5cddc7e015578d8b4d47436bd734dc699c20
Ewin <ewintec@163.com>
2025-06-24 11:30:31 +0800

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -74,6 +74,11 @@ export const HOSTS_PATH = "hosts"
 
 export const DNS_PROVIDERS = [
   {
+    url: (domain: string) => `https://101.102.103.104/dns-query?name=${domain}&type=A`,
+    headers: { Accept: "application/dns-json" },
+    name: "TWQuad101 DNS",
+  },  
+  {
     url: (domain: string) => `https://1.1.1.1/dns-query?name=${domain}&type=A`,
     headers: { Accept: "application/dns-json" },
     name: "Cloudflare DNS",
